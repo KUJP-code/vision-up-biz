f0b9bd178046d65db8e5ce22c4138b7feeccb484
f-michael-95 <f-michael@kids-up.jp>
2024-12-09 16:53:01 +0900

    ALL CHANGES

--- success.php
+++ success.php
@@ -17,6 +17,8 @@
   </div>
   
   <div class="container">
+
+  <img src="./includes/images/success/success-top.webp" alt="" class="generic-page-top-left">
     <p>VISION UPはKids UP事業成功を背景に、志を共にする企業に向けて私たちが日常的に提供する教育サービスに必要なツールや知識を全て提供します。私たちの英会話スクール事業の始まりは、辻球一、石原佐和子、そしてジャック・ハンの3名によって、株式会社P-UPの子会社として2016年に学童保育型英会話スクール「Kids UP」を立ち上げました。彼らは日本の英会話スクール業界で豊富な経験を持ち、本当に使える英語力を子ども達に身に付けさせる洗練された英会話スクールを作りあげることを構想しました。彼らの目標は英語漬けの環境で留学にも似た長時間英語漬けの環境を過ごすと同時に、共働きの保護者に向けて放課後の預かり先を提供することも目的としていました。彼らのビジョンは、3つの考え方に基づきます。：<br>
       <br>
       <b>子どもたちに生き抜く力を育む：</b> 私たちは子ども達が何事にもチャレンジすることを大事にしています。将来の子ども達の成功に不可欠である自信をつけさせることを目的とした育成環境に努めています。私たちが作るプログラムは好奇心を刺激し、向学心を育み、子どもたちが自信と弾力性をもって将来の課題に取り組む準備ができるように設計されています。<br>
@@ -28,6 +30,85 @@
       私たちの始まりは、2016年の第1号店である田園調布雪谷校から始まりました。この田園調布雪谷校での成功により、東京都大田区に新校をオープンすることにつながりました。私たちの成長の勢いは衰えることなく、2024年現在では東京、神奈川、千葉、埼玉に34校を展開するまでになっています。現在、Kids UPは5,257人の生徒と500人以上のスタッフまでに拡大し、充実した英語教育の提供に貢献しています。</p>
   </div>
 
+
+  <div class="swiper" id="testimony-carousel">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide swiper-slide-active">
+        <figure>
+          <div class="speech-bubble active">
+            <p class="testimony-text active">Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+          </div>
+          <p class="testimony-name active">Yukihiro Tanaka</p>
+          <p class="testimony-title active">Marketing Director, ZenithTech Solutions</p>
+        </figure>
+      </div>
+      <!-- End Slide 1 -->
+      <div class="swiper-slide">
+        <figure>
+          <div class="speech-bubble">
+            <p class="testimony-text">Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+          </div>
+          <p class="testimony-name">Yukihiro Tanaka</p>
+          <p class="testimony-title">Marketing Director, ZenithTech Solutions</p>
+        </figure>
+      </div>
+      <!-- End Slide 2 -->
+
+      <div class="swiper-slide">
+        <figure>
+          <div class="speech-bubble">
+            <p class="testimony-text">Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+          </div>
+          <p class="testimony-name">Yukihiro Tanaka</p>
+          <p class="testimony-title">Marketing Director, ZenithTech Solutions</p>
+        </figure>
+      </div>
+      <!-- End Slide 3 -->
+
+      <div class="swiper-slide">
+        <figure>
+          <div class="speech-bubble">
+            <p class="testimony-text">Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+          </div>
+          <p class="testimony-name">Yukihiro Tanaka</p>
+          <p class="testimony-title">Marketing Director, ZenithTech Solutions</p>
+        </figure>
+      </div>
+      <!-- End Slide 4 -->
+      <!-- Other slides remain the same -->
+    </div>
+  
+    <div class="nav-arrows-wrapper d-flex align-items-center">
+      <img
+      src="./includes/images/global/carousel-arrow-left.svg"
+        alt="arrow-left"
+        class="swiper-custom-nav-arrow testimony-swiper-button-left"
+      />
+      <img
+        src="./includes/images/global/carousel-arrow-right.svg"
+        alt="arrow-right"
+        class="swiper-custom-nav-arrow testimony-swiper-button-right"
+      />
+    </div>
+  </div>
+
+  <div class="container">
+    <p>"2023年にはKids UPは売上高20億円を突破し、学童保育型英会話スクール業界トップの地位を確立しました。しかし、これまでの道のりに困難がなかったわけではありません。コロナウィルスが世界的に大流行した際、2ヶ月間の全校一斉休校という厳しい決断に迫られました。しかしながら、戦略的な企業経営、そして理念へのコミットメントを通して、Kids UPはこの危機を乗り切るだけでなく、より経営基盤を盤石としました。コロナ禍で社会全体が自粛ムードを取り続ける中、スクール運営に併せて学期休み中限定のシーズナルスクール（午前レッスン）も通常通り開催、2020年末までに2校の新規開校も行いました。コロナ禍真っ只中においても大幅な売上増を達成し、当社のビジネスモデルの堅牢性を実証しました。<br>
+<br>
+      創立メンバーによって提唱された基本原則に則った運営方針が、私たちの成功の基盤となっています。基本に忠実な一方で時代の変化に併せて絶えず手法を改良してきました。会社が拡大するにつれ、多様なスキルと専門知識を持つ多くの人材が入社してきたことにより、Kids UP独自のカリキュラム開発が実現しました。このカリキュラムには、工作や運動、知育などさまざまなアクティビティに加え、厳選された副教材が盛り込まれています。英語力を培うことは勿論のこと、迅速かつ効率的にレッスン準備ができるよう、現場スタッフを支えています。<br>
+      <br>
+      このような変化へのコミットメントが、他社とは一線を画すカリキュラム開発につながっています。カリキュラム開発に加え、学習しやすい環境を作るために、最先端の設備投資をしています。教室には最新のテクノロジーが導入され、様々な学習スタイルに対応した魅力的な授業を可能にしています。<br>
+      <br>
+      さらに私たちは、地域社会と保護者が子どもの教育に関わることの重要性を強く意識しています。定期的にワークショップやイベントなどを通じて保護者と積極的に関わり、保護者も子ども達の教育パートナーであることを常に訴えています。このような総合的なアプローチにより、各スクール周辺では協力的なコミュニティが形成されています。<br>
+      <br>
+      VISION UPは将来に向けて、日本全国の多くの地域に私たちが誇る教育サービスをお届けすることを目指します。私たちは継続的にプログラムを改善し、新しい教育方法を取り入れ、学習体験を向上させるためテクノロジー活用にコミットします。そして、子ども達とそのご家族の人生に大きな影響を与え、学業や大人になってからも学ぶことへの情熱を育むことが私たちの目標です。<br>
+      <br>
+
+      VISION UPでは、私たちの実績と成功してきたビジネスモデルを採用し、英語教育の発展に協力いただけるパートナーを募集しています。私たちの革新的なプログラムは、従来の教育を超えた広範囲で豊かな体験を子ども達に提供するデザインとなっています。そして、企業パートナーの皆様が私たちのサービスを容易に使用していただけるように、さまざまなトレーニングやツールをご用意しています。私たちが確立した学童保育型英会話スクールのサービスを通じて、ビジネスのすそ野を広げ、次世代の人材育成に共に汗を流しましょう。
+</p>
+<img src="./includes/images/success/success-bottom.webp" alt="" class="generic-page-bottom-right">
+
+  </div>
 
 <?php include './includes/footers/footer-ja.php'?>
 
